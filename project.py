4894538d30ca40fd2bf6828495341bf7710f51c4
neerajpokala143 <73011282+neerajpokala143@users.noreply.github.com>
2021-12-23 11:39:24 +0530

    Update project.py

--- project.py
+++ project.py
@@ -381,3 +381,106 @@ if app_mode =='Translator':
 
     elif latin_button:
         st.error("!! Please enter input in any language")
+        
+        
+        
+        
+ if app_mode=='Face Recognization':
+  st.image(os.path.join('./images','facial-recognition-software-image.jpg'),use_column_width=True )
+  st.title("Face Recognition(Powered by Azure)")
+  st.header('Face Recognition:')
+  st.markdown("Using Azure I build to detect, identify and analyse faces in images.")
+  st.text("Detect the objects in images")
+  
+  image_file =  st.file_uploader("Upload Images (less than 1mb)", type=["png","jpg","jpeg"])
+  if image_file is not None:
+    img = Image.open(image_file)
+    st.image(image_file,width=250,caption='Uploaded image')
+    byte_io = BytesIO()
+    img.save(byte_io, 'PNG')#PNG
+    image = byte_io.getvalue()
+
+
+  button_translate=st.button('Click me',help='To give the image')
+
+  if button_translate and image_file :
+
+    def draw_face(img):
+
+        subscription_key = 'ea8c44f876804e43ab35a26a09d59da5'  
+        BASE_URL = "https://recognition-ai.cognitiveservices.azure.com/" + '/face/v1.0/detect'  
+        headers = {
+        # Request headers
+        'Content-Type': 'application/octet-stream', 
+        'recognitionModel': 'recognition_01' ,
+        'Ocp-Apim-Subscription-Key': subscription_key,
+        'detectionModel': 'detection_01',
+        }
+        response = requests.post(BASE_URL, headers=headers, data=image)
+        faces = response.json()
+        print(faces)
+        def getRectangle(faceDictionary):
+            rect = faceDictionary['faceRectangle']
+            left = rect['left']
+            top = rect['top']
+            bottom = left + rect['height']
+            right = top + rect['width']
+            return ((left, top), (bottom, right))
+
+
+        output_image = Image.open(BytesIO(image))
+        
+    #   For each face returned use the face rectangle and draw a red box.
+        draw = ImageDraw.Draw(output_image)
+        for face in faces:
+            draw.rectangle(getRectangle(face), outline='red',width=2)
+        return output_image
+   #image_data = open(image_file, "rb").read()
+
+    image = draw_face(img)
+
+    st.image(image, caption='Output image')
+    
+    
+    
+if app_mode =='Object Detection':
+    
+    st.image(os.path.join('./images','object.jpg'),use_column_width=True )
+    st.markdown("<h1 style='text-align: center; color: skyblue; '> Object Recognition </h1>", unsafe_allow_html=True)
+
+    st.title("Object Recognition(Powered by Azure)")
+
+    st.markdown("Using Azure I build to **_Object_ detection** , it identify and analyse the image.")
+    st.text("Detect the objects in images")
+
+    url_file =  title = st.text_input('Paste image address URL')
+    st.text("Example :- https://www.intelligentliving.co/wp-content/uploads/2019/12/sculptures-reclaimed-materials-brianmock8_201911323127.jpg")
+    button_translate=st.button('Click me',help='To give the image')
+
+    if button_translate and url_file :
+   
+        subscription_key = 'afac470736ce49ca8352ec7c83736fc7'
+        endpoint = 'https://objectdetection21.cognitiveservices.azure.com/'
+        
+            # Add your Computer Vision subscription key and endpoint to your environment variables.
+        analyze_url = endpoint + "vision/v3.1/analyze"
+        
+        # Set image_url to the URL of an image that you want to analyze.
+        headers = {'Ocp-Apim-Subscription-Key': subscription_key}
+        params = {'visualFeatures': 'Categories,Description,Color'}
+        data = {'url': url_file}
+        response = requests.post(analyze_url, headers=headers,params=params, json=data)
+        response.raise_for_status()
+
+        # The 'analysis' object contains various fields that describe the image. The most
+        # relevant caption for the image is obtained from the 'description' property.
+        analysis = response.json()
+        print(json.dumps(response.json()))
+        image_caption = analysis["description"]["captions"][0]["text"].capitalize()
+        response_image = requests.get(url_file)
+        
+        # Display the image and overlay it with the caption.
+        aux_im = Image.open(BytesIO(response_image.content))
+        captio=st.subheader(image_caption)
+        st.image(aux_im, caption=image_caption)
+
